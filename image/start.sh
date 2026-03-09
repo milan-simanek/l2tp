3452ab58450693c0d18b7df598c91294aa54f140
Milan Simanek <milan@componentsoft.eu>
2026-03-10 00:50:54 +0100

    image files

---
+++ image/start.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+cleanup() {
+  kill `cat /run/xl2tpd.pid`
+  pkill syslogd
+  exit
+}
+
+trap "cleanup" TERM
+
+syslogd
+
+# Keep the tunnel interface up
+while sleep 1
+do
+    if [ ! -f /run/xl2tpd.pid ] \
+    || ! read xpid < /run/xl2tpd.pid \
+    || [ -z "$xpid" ] \
+    || ! [ -d /proc/$xpid ]
+    then
+      echo "starting xl2tpd..."
+      xl2tpd
+      continue
+    fi
+
+    sleep 10
+
+    [ -f /run/ifname ] \
+    && read ifname < /run/ifname \
+    && [ -n "$ifname" ] \
+    && ip link show "$ifname" | grep -q '\<UP\>' && continue
+
+    sed -n 's/^[[:space:]]*\[ *lac  *\([^[:space:]][^[:space:]]*\)[[:space:]]*\].*$/\1/p' \
+        /etc/xl2tpd/xl2tpd.conf \
+    | while read name x
+    do
+      echo "reconnecting $name"
+#      echo "c $name" > /var/run/xl2tpd/l2tp-control
+      xl2tpd-control connect-lac "$name"
+      break
+    done
+done
